6cc4716a4aab3c427e4f1dcb74cf433ebe5ef3b0
developer <developer@developer.com>
2023-07-21 14:52:34 +0530

    Added Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable, :trackable, :timeoutable
+  validate :age_must_be_eighteen_or_above
+
+  private
+
+  def age_must_be_eighteen_or_above
+    if birth_date.present? && birth_date >= 18.years.ago.to_date
+        errors.add(:birth_date, 'Age should be over 18 years old')
+    end
+  end
 end
