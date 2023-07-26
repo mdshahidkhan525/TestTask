2d782ec112b7a6b35042aaf2b723505f2986c959
developer <developer@developer.com>
2023-07-26 16:00:10 +0530

    Uses custom validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with UserValidator
+
   attr_accessor :login
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable, :trackable, :timeoutable
 
-  validate :age_must_be_eighteen_or_above
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-  validate :validate_username
-
 
   def login
     @login || self.username || self.email
@@ -27,12 +27,6 @@ class User < ApplicationRecord
       else
         where(username: conditions[:username]).first
       end
-    end
-  end
-
-  def age_must_be_eighteen_or_above
-    if birth_date.present? && birth_date >= 18.years.ago.to_date
-        errors.add(:birth_date, 'Age should be over 18 years old')
     end
   end
 end
