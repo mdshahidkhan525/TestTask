d298a9fae49d4e363d966a0eb834be468f91c3ed
developer <developer@developer.com>
2023-07-26 17:33:53 +0530

    Update navigation

--- app/helpers/user_helper.rb
+++
@@ -1,5 +0,0 @@
-module UserHelper
-  def full_name(user)
-    "#{user.first_name} #{user.last_name}"
-  end
-end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def full_name(user)
+    "#{user.first_name} #{user.last_name}"
+  end
+
+  def request_count(user)
+    user.follow_requests.count
+  end
 end
