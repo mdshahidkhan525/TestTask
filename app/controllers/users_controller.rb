beaca0d4102bc8ce43eb2cc00900e217276d3d1f
developer <developer@developer.com>
2023-07-27 13:37:46 +0530

    Added Search functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def search_users
+    query = params[:query]
+    @users = User.search(query)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  devise_scope :user do
-    get '/signin', to: 'devise/sessions#new'
-    get '/signup', to: 'devise/registrations#new'
-  end
-
   root 'feeds#index'
+  draw :users
 end
