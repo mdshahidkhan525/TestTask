bfdbc916b3450a6d498a9cb63a7ec20c734cb57e
developer <developer@developer.com>
2023-07-27 11:46:04 +0530

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users/registrations"}
-  root 'homes#index'
+  devise_scope :user do
+    get '/signin', to: 'devise/sessions#new'
+    get '/signup', to: 'devise/registrations#new'
+  end
+
+  root 'feeds#index'
 end
