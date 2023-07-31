f39b58781b86bc885dc780d9a85d3417521feaf3
developer <developer@developer.com>
2023-07-31 13:14:19 +0530

    Update requests

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class FollowersController < ApplicationController
-  before_action :find_user, only: %i[send_follow_request block_user]
+  before_action :find_user, only: %i[send_follow_request block_user accept_request reject_request]
 
   def send_follow_request
     current_user.send_follow_request_to(@user)
@@ -7,6 +9,26 @@ class FollowersController < ApplicationController
 
   def block_user
     current_user.block(@user)
+  end
+
+  def follow_requests
+    @follow_requests = current_user.follow_requests
+  end
+
+  def accept_request
+    if current_user.accept_follow_request_of(@user)
+      redirect_to requests_path, notice: 'Follow request accepted successfully'
+    else
+      redirect_to requests_path, alert: 'Failed to accept follow request'
+    end
+  end
+
+  def reject_request
+    if current_user.decline_follow_request_of(@user)
+      redirect_to requests_path, notice: 'Follow request rejected successfully'
+    else
+      redirect_to requests_path, alert: 'Failed to reject follow request'
+    end
   end
 
   private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,10 +2,18 @@
 
 module UsersHelper
   def full_name(user)
-    "#{user.first_name} #{user.last_name}"
+    "#{user.first_name.capitalize} #{user.last_name.capitalize}"
   end
 
   def request_count(user)
     user.follow_requests.count
+  end
+
+  def user_name(user)
+    user.username
+  end
+
+  def user_followers(user)
+    user.followers.count
   end
 end
