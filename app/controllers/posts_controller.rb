31b6e2a562c468daa5a0bca1f4452ff4663c4b5c
developer <developer@developer.com>
2023-08-04 16:50:09 +0530

    minor fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,36 @@
 
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  after_action :is_current_user_post?, only: %i[edit]
+  before_action :find_post, only: %i[edit update destroy]
 
   def create
     Post.create(post_params)
     redirect_to '/'
   end
 
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to profile_path
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def is_current_user_post?
+    @post.user.eql?current_user
+  end
 
   def post_params
     params.require(:post).permit(:title, :description).merge(user_id: current_user.id)
